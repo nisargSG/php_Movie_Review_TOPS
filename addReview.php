17ba1f2ced5db676ea291bc48922f6d5c31e8267
nisargSG <nisargsg.tops@gmail.com>
2024-03-14 12:05:39 +0530

    -posting reviews

--- addReview.php
+++ addReview.php
@@ -7,6 +7,7 @@ include "checkUserAuth.php";
 checkUserAuth();
 
 $movieId=null;
+$showError = false;
 
 if($_GET["movieId"]){
     $movieId=$_GET["movieId"];
@@ -17,13 +18,42 @@ else{
 
 ?>
 
+<?php
+     if($_SERVER['REQUEST_METHOD']=="POST"){
+
+        //receieve rating and review
+
+        if($_POST["movie_id"] && $_POST["rating"] && $_POST["review"]){
+            //insert
+
+            $movieId=$_POST["movie_id"];
+            $rating = $_POST["rating"];
+            $review = $_POST["review"];
+
+            $sql = "INSERT INTO review(movie_id,user_id,rating,comment) VALUES('$movieId','$userId','$rating','$review')";
+            if(!($dbConn->query($sql)===TRUE)){
+                $showError=true;
+            }
+            else{
+                header("Location:dashboard.php");
+            }
+
+
+        }
+
+
+     }
+
+
+?>
+
 
 <html>
 
     <head>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/dashboard.css">
+        <link rel="stylesheet" href="assets/css/addReview.css">
 
     </head>
 
@@ -34,14 +64,62 @@ else{
             $moviesResult = $dbConn->query($sql);
             $movieRow = $moviesResult->fetch_assoc();
 
-            echo ($movieRow["title"]); 
-            echo ($movieRow["description"]); 
-            echo ($movieRow["release_date"]); 
-            echo ($movieRow["image"]); 
-
-
         ?>
 
+        <div class="jumbotron">
+            
+            <div class="row">
+
+                <div class="col-lg-6 ">
+                    <img class="img_movie" src="assets/img/img_movies/<?php echo($movieRow["image"])  ?>" />
+                </div>
+                <div class="col-lg-6">
+                    <h3><?php echo($movieRow["title"]) ?></h3>
+                    <p><b><?php echo($movieRow["release_date"]) ?></b></p>
+                    <p><?php echo($movieRow["description"]) ?></p>
+                    <hr/>
+                    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <input type="hidden" name="movie_id" value="<?php echo($movieId) ?>"/>
+                        <div class="form-group">
+                            <label for="ratings_dropdown">Select Ratings:</label>
+                            <select name="rating" id="ratings_dropdown">
+                                <option value="1">1/5</option>
+                                <option value="2">2/5</option>
+                                <option value="3">3/5</option>
+                                <option value="4">4/5</option>
+                                <option value="5">5/5</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="review">Add Your Review</label>
+                            <textarea name="review" class="form-control" rows="5" id="review" required></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-warning">Post My Review</button> 
+                        <a href="dashboard.php" class="btn btn-danger">Cancel</a> 
+
+                    </form>
+
+
+                    <?php
+
+                    if ($showError == true) { ?>
+
+                    <div class="alert alert-danger">
+                        <strong>Failed!</strong> Couldn't Save Your Review , Please Try Again
+                    </div>
+
+                    <?php
+                        }
+
+                    ?>
+
+
+                    
+                </div>
+
+            </div>
+
+        </div>
 
 
     </body>
